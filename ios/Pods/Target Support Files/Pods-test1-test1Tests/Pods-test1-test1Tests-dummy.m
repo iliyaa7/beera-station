3f189206345e93776584eccef4e310ee9492e372
Iliya Gomon <iliya.gomon@msapps.mobi>
2022-06-15 12:20:23 +0300

    git ignore

--- ios/Pods/Target Support Files/Pods-test1-test1Tests/Pods-test1-test1Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_test1_test1Tests : NSObject
-@end
-@implementation PodsDummy_Pods_test1_test1Tests
-@end
--- ios/Pods/Target Support Files/idenfy-react-native-sdk/idenfy-react-native-sdk-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_idenfy_react_native_sdk : NSObject
-@end
-@implementation PodsDummy_idenfy_react_native_sdk
-@end
